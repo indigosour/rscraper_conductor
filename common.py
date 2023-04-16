d5828ca420eb8612827971f9e82e685af6aa1a9d
indigosour <48144817+indigosour@users.noreply.github.com>
2023-04-16 08:46:20 +0000

    Minor updates

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-import os,json,emoji,re,logging,praw,shutil,glob
+import os,json,emoji,re,logging,praw,shutil,glob,prawcore,time
 from azure.keyvault.secrets import SecretClient
 from azure.identity import ClientSecretCredential
 
@@ -80,3 +80,39 @@ def clear_tmp_folder():
                 shutil.rmtree(f)
         except OSError as e:
             print(f"Error: {e.filename} - {e.strerror}")
+
+
+def get_reddit_posts(sub, period):
+    retry_count = 0
+    max_retries = 5
+
+    while retry_count < max_retries:
+        try:
+            with reddit_auth() as reddit:
+                posts = reddit.subreddit(sub).top(
+                    time_filter=period,
+                    limit=1000
+                )
+            break  # If the request is successful, break out of the loop
+        except prawcore.exceptions.ResponseException as e:
+            if e.response.status_code == 503:
+                print(f"Caught a 503 error: {e}.")
+                
+                # Print response headers
+                print("Response headers:", e.response.headers)
+                # Check for 'retry_after' header
+                retry_after = int(e.response.headers.get('retry-after', 5))
+                print(f"Retrying in {retry_after} seconds...")
+                
+                time.sleep(retry_after)
+                retry_count += 1
+            else:
+                raise
+        except Exception as e:
+            raise
+
+    if retry_count == max_retries:
+        print("Failed to complete the request after maximum retries")
+        return None
+
+    return posts
